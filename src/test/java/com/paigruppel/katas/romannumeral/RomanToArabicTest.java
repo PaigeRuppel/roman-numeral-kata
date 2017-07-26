601ad344ce55889bfba681ead2fa195bc1c03950
Paige Ruppel <paige.ruppel@gmail.com>
2017-07-26 19:29:56 -0400

    refactored tests

--- src/test/java/com/paigruppel/katas/romannumeral/RomanToArabicTest.java
+++ src/test/java/com/paigruppel/katas/romannumeral/RomanToArabicTest.java
@@ -1,28 +1,33 @@
 package com.paigruppel.katas.romannumeral;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class RomanToArabicTest {
-	
+
+	RomanToArabic underTest;
+
+	@Before
+	public void setup() {
+		underTest = new RomanToArabic();
+	}
+
 	@Test
 	public void shouldReturn1() {
-		RomanToArabic underTest = new RomanToArabic();
 		int response = underTest.convertToArabic("I");
 		assertEquals(response, 1);
 	}
-	
+
 	@Test
 	public void shouldReturn2() {
-		RomanToArabic underTest = new RomanToArabic();
 		int response = underTest.convertToArabic("II");
 		assertEquals(response, 2);
 	}
-	
+
 	@Test
 	public void shouldReturn3() {
-		RomanToArabic underTest = new RomanToArabic();
 		int response = underTest.convertToArabic("III");
 		assertEquals(response, 3);
 	}
