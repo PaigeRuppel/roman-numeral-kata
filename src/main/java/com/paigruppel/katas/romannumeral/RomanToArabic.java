597109b00922ccc7260bbdcec03781143b14df8c
Paige Ruppel <paige.ruppel@gmail.com>
2017-07-29 16:01:50 -0400

    refactored tests and method

--- src/main/java/com/paigruppel/katas/romannumeral/RomanToArabic.java
+++ src/main/java/com/paigruppel/katas/romannumeral/RomanToArabic.java
@@ -5,41 +5,51 @@ import java.util.Map;
 
 public class RomanToArabic {
 
-	public int convertToArabic(String input) {
-		
-		char[] inputArray = input.toCharArray();
-		
-		int[] rawValues = new int[inputArray.length];
-		
-		for (int i = 0 ; i < rawValues.length; i++) {
-			rawValues[i] = romanArabicMap().get(inputArray[i]);
-		}
-		
+	private String input;
+
+	private char[] inputArray;
+
+	private int[] rawValues;
+
+	public RomanToArabic(String input) {
+		this.input = input;
+		inputArray = input.toCharArray();
+		rawValues = convertToRawValues();
+	}
+
+	public int convertToArabic() {
 
 		int first = 0;
 		int response = rawValues[0];
-		
-		for (int next = 1; next < inputArray.length; next++) {
-			if (isDecreasing(rawValues, first, next)) {
-				response =  response + rawValues[next];	
+
+		for (int next = 1; next < rawValues.length; next++) {
+			if (isDecreasing(first, next)) {
+				response = response + rawValues[next];
 				first++;
 			} else {
 				response = (response - rawValues[first]) + (rawValues[next] - rawValues[first]);
 				first++;
 			}
 		}
-		
 		return response;
-
 	}
 
-	private boolean isDecreasing(int[] rawValues, int first,  int next) {
+	private int[] convertToRawValues() {
+		int[] rawValues = new int[inputArray.length];
+
+		for (int i = 0; i < rawValues.length; i++) {
+			rawValues[i] = romanArabicMap().get(inputArray[i]);
+		}
+		return rawValues;
+	}
+
+	private boolean isDecreasing(int first, int next) {
 		return rawValues[next] <= rawValues[first];
 	}
 
 	private static Map<Character, Integer> romanArabicMap() {
-		Map <Character, Integer> romanArabic = new HashMap<>();
-		
+		Map<Character, Integer> romanArabic = new HashMap<>();
+
 		romanArabic.put('I', 1);
 		romanArabic.put('V', 5);
 		romanArabic.put('X', 10);
@@ -47,6 +57,7 @@ public class RomanToArabic {
 		romanArabic.put('C', 100);
 		romanArabic.put('D', 500);
 		romanArabic.put('M', 1000);
+
 		return romanArabic;
 	}
 
--- src/test/java/com/paigruppel/katas/romannumeral/RomanToArabicTest.java
+++ src/test/java/com/paigruppel/katas/romannumeral/RomanToArabicTest.java
@@ -8,85 +8,80 @@ import org.junit.Test;
 
 public class RomanToArabicTest {
 
-	RomanToArabic underTest;
 
-	@Before
-	public void setup() {
-		underTest = new RomanToArabic();
+
+	public RomanToArabic underTest(String input) {
+		return new RomanToArabic(input);
 	}
 
 	@Test
 	public void shouldReturn1() {
-		int response = underTest.convertToArabic("I");
-		assertThat(response, is(1));
+		assertThat(underTest("I").convertToArabic(), is(1));
 	}
 
 	@Test
 	public void shouldReturn2() {
-		int response = underTest.convertToArabic("II");
-		assertThat(response, is(2));
+		assertThat(underTest("II").convertToArabic(), is (2));
 	}
 
 	@Test
 	public void shouldReturn3() {
-		int response = underTest.convertToArabic("III");
-		assertThat(response, is(3));
+		assertThat(underTest("III").convertToArabic(), is (3));
 	}
 
 	@Test
 	public void shouldReturn4() {
-		int response = underTest.convertToArabic("IV");
-		assertThat(response, is(4));
+		assertThat(underTest("IV").convertToArabic(), is (4));
 	}
 	
 	@Test
 	public void shouldReturn5() {
-		assertThat(underTest.convertToArabic("V"), is(5));
+		assertThat(underTest("V").convertToArabic(), is(5));
 	}
 	
 	@Test
 	public void shouldReturn6() {
-		assertThat(underTest.convertToArabic("VI"), is(6));
+		assertThat(underTest("VI").convertToArabic(), is(6));
 	}
 	
 	@Test
 	public void shouldReturn7() {
-		assertThat(underTest.convertToArabic("VII"), is(7));
+		assertThat(underTest("VII").convertToArabic(), is(7));
 	}
 	
 	@Test
 	public void shouldReturn8() {
-		assertThat(underTest.convertToArabic("VIII"), is(8));
+		assertThat(underTest("VIII").convertToArabic(), is(8));
 	}
 	
 	@Test
 	public void shouldReturn9() {
-		assertThat(underTest.convertToArabic("IX"), is(9));
+		assertThat(underTest("IX").convertToArabic(), is(9));
 	}
 	
 	@Test
 	public void shouldReturn14() {
-		assertThat(underTest.convertToArabic("XIV"), is(14));
+		assertThat(underTest("XIV").convertToArabic(), is(14));
 	}
 	
 	@Test
 	public void shouldReturn24() {
-		assertThat(underTest.convertToArabic("XXIV"), is(24));
+		assertThat(underTest("XXIV").convertToArabic(), is(24));
 	}
 	
 	@Test
 	public void shouldReturn44() {
-		assertThat(underTest.convertToArabic("XLIV"), is(44));
+		assertThat(underTest("XLIV").convertToArabic(), is(44));
 	}
 	
 	@Test
 	public void shouldReturn544() {
-		assertThat(underTest.convertToArabic("DXLIV"), is(544));
+		assertThat(underTest("DXLIV").convertToArabic(), is(544));
 	}
 	
 	@Test
 	public void shouldReturn1144() {
-		assertThat(underTest.convertToArabic("MCXLIV"), is(1144));
+		assertThat(underTest("MCXLIV").convertToArabic(), is(1144));
 	}
 
 }
